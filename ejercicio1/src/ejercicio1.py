07aebf87dbfa48c2a6f84cc4c4ecb1ff5830f092
Alumno <Alumno@LABSI-11>
2015-10-13 10:46:06 -0500

    ejercicio 1

---
+++ ejercicio1/src/ejercicio1.py
@@ -0,0 +1,10 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+### Introducir un nuumero por teclado y decir si es par o impar
+h = input('Introduzca un numero: ')
+if h%2 == 0:
+    print 'Este numero es par'
+else:
+    print 'Este numero es impar'    
